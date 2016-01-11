b164e991923750c2a6b93c9f6c8ea72668043bb2
Marie Ljus <marie.ljus@e-man.se>
2016-01-11 12:49:17 +0100

    blaj2

--- MySecondWebJob/Program.cs
+++ MySecondWebJob/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Azure.WebJobs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,23 @@ namespace MySecondWebJob
     {
         static void Main(string[] args)
         {
+            var config = new ServiceBusConfiguration();
+
+            // Connection String Service Bus
+            config.ConnectionString =
+                "Endpoint=sb://testdemo-dev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=VUyEwog5NpmezTHMZBT04NdqjsiXujP317NbcGDTwQw=";
+
+            var jobHostConfiguration = new JobHostConfiguration();
+            jobHostConfiguration.UseServiceBus(config);
+
+            // Connection String / Primarey Key Web App
+            jobHostConfiguration.DashboardConnectionString =
+                "DefaultEndpointsProtocol=https;AccountName=enkeltftpdemo;AccountKey=vcnrOgpgVws1xfziK9unuUabmqaTAg2B+sH/8ndcFCNS7YTdlYklyiFcvZx6MplxFjELEEfY/YC1U38pwkHzfQ==";
+            jobHostConfiguration.StorageConnectionString =
+                "DefaultEndpointsProtocol=https;AccountName=enkeltftpdemo;AccountKey=vcnrOgpgVws1xfziK9unuUabmqaTAg2B+sH/8ndcFCNS7YTdlYklyiFcvZx6MplxFjELEEfY/YC1U38pwkHzfQ==";
+
+            var jobhost = new JobHost(jobHostConfiguration);
+            jobhost.RunAndBlock(); // Continous
         }
     }
 }
